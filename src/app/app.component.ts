a6e5d382db5631fba571e4f94358522306d90ce3
Agustin Juan <agustinchoslr@gmail.com>
2022-04-15 19:52:31 -0300

    primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,58 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ahorcado';
+
+  palabra ='AGUACATE';
+  palabraOculta ='_ ';
+  intentos = 0;
+
+  gano = false;
+  perdio= false;
+
+letras = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 
+'N', 'Ñ', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+
+  constructor(){
+    
+    this.palabraOculta = '_ '.repeat(this.palabra.length)
+  }
+  comprobar( letra: any ){
+    this.existeLetra(letra);
+
+    const palabraOcultaArr = this.palabraOculta.split(' ');
+    for(let i = 0; i<this.palabra.length; i++){
+      if (this.palabra[i] === letra){
+        palabraOcultaArr[i] = letra;
+      }
+    }
+
+    this.palabraOculta = palabraOcultaArr.join(' ');
+    this.verificaGane();
+
+  }
+
+  verificaGane(){
+
+    const palabraArr = this.palabraOculta.split( ' ');
+     const palabraEvaluar = palabraArr.join('');
+
+     if (palabraEvaluar === this.palabra ) {
+       this.gano = true;
+     }
+
+     if ( this.intentos >= 9 ) {
+
+      this.perdio = true; 
+    }
+  }
+  
+  
+  existeLetra( letra: any ){
+    if( this.palabra.indexOf(letra) >= 0 ) {
+      //encontró la letra
+    }
+    else {
+      this.intentos ++;
+    }
+  }
 }
